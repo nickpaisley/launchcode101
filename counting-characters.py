23656f6710cf5d2b5e2347f60d8385097ac686bd
np303 <2ep303@gmail.com>
2019-03-05 16:16:39 -0600

    go!

--- counting-characters.py
+++ counting-characters.py
@@ -3,7 +3,7 @@
 #created - 3.4.19
 
 #Write a program that calculates the number of times each character occurs in a string and prints these stats to the console.
-#Here’s a test string, for your convenience
+#Here’s a test string, for your conveniencepopp
 
 test_string = "234r"
 
