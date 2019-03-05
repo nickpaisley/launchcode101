98d11478d822aba14c4b455bc57af090ff9377b0
np303 <2ep303@gmail.com>
2019-03-05 15:57:09 -0600

    so far

--- counting-characters.py
+++ counting-characters.py
@@ -5,9 +5,15 @@
 #Write a program that calculates the number of times each character occurs in a string and prints these stats to the console.
 #Here’s a test string, for your convenience
 
-test_string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus."
+test_string = "CCBBAA"
 
-def char_times(test_string): 
-    times = ""
+char_dict = {}
+def char_add(test_string): 
     for char in test_string:
-        times = times + char(0)
+        char_dict[char] = test_string.count(char)
+    return (char_dict)
+    
+
+char_add(test_string)
+for keys in (char_dict):
+    print(keys)
