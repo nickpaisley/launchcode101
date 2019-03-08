97e7f1131e2128c7cf3624b280a31577f835c8c8
np303 <2ep303@gmail.com>
2019-03-08 09:37:12 -0600

    final

---
+++ country_code_price_count.py
@@ -0,0 +1,11 @@
+def get_country_codes(prices):
+    return ",".join([item.split("$")[0] for item in prices.split(", ")])
+
+
+def main():
+    prices = ("NZ$300, KR$1200, DK$5")
+    print(get_country_codes(prices))
+    
+
+if __name__ == '__main__':
+    main()
